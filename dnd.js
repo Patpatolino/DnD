a2895871cd3445515e6c82e5db9150eb13e505f5
Patrick Anlanger <anlanger.patrick@gmail.com>
2019-10-19 01:30:41 +0200

    aufgeräumt

--- dnd.js
+++ dnd.js
@@ -1,10 +1,3 @@
-document.write(
-    '<script src="http://' +
-    (location.host || '${1:localhost}').split(':')[0] +
-    ':${2:35729}/livereload.js?snipver=1"></' +
-    'script>'
-);
-
 class Player {
     // constructor(name, dice) {
     //     this.name = name;
@@ -16,16 +9,10 @@ class Player {
     }
 
     roll(dice) {
-        
         alert(dice);
     }
 }
 
-let createCharakterBtn = document.getElementById('createCharakterBtn');
-createCharakterBtn.addEventListener('click', function () {
-    let player = new Player();
-    player.roll();
-})
 
 for (i = 1; i < 26; i++) {
     let playerRolls = document.getElementById('p' + i);
@@ -36,42 +23,3 @@ for (i = 1; i < 26; i++) {
     })
 }
 
-
-
-// let playerOneButtons = document.getElementById('p'+1);
-// playerOneButtons.addEventListener('click', function() {
-//     let player = new Player();
-//     player.roll(parseInt(this.innerHTML));
-// })
-
-
-
-
-// class Dice {
-//     player;
-//     eyes;
-//     constructor(player, eyes) {
-//         this.player = player;
-//         this.eyes = eyes;
-//     }
-
-//     player(player) {
-//         addEventListener.document.getElementById(player)('click', function () {
-//             alert(player);
-//         })
-//     }
-
-//     static eyes;
-
-//     static roll() {
-//         alert(Math.floor((Math.random() * 20) + 1));
-//     }
-// }
-
-
-
-// let asdfBtn = document.getElementById('p1d20');
-// asdfBtn.addEventListener('click', function () {
-//     let d = new Dice(1, 1);
-//     d.roll();
-// });
