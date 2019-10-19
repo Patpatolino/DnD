5d1f4a139c5f2bd6b25e5f98bb804c615d2ccb97
Patrick Anlanger <anlanger.patrick@gmail.com>
2019-10-19 12:01:14 +0200

    noch mehr aufgeräumt

--- dnd.js
+++ dnd.js
@@ -1,25 +1,20 @@
 class Player {
-    // constructor(name, dice) {
-    //     this.name = name;
-    //     this.dice = dice;
-    // }
-
-    name(name) {
-        alert(name);
-    }
-
-    roll(dice) {
-        alert(dice);
+    constructor(name, roll) {
+        this.playerName = name;
+        this.rollResult = getRandomInt(roll);
     }
 }
-
 
 for (i = 1; i < 26; i++) {
     let playerRolls = document.getElementById('p' + i);
     playerRolls.addEventListener('click', function () {
-        let player = new Player();
-        player.name(this.name);
-        player.roll(parseInt(this.innerHTML));
+        let player = new Player(this.name, parseInt(this.innerHTML));
+        console.log(player);
     })
 }
 
+function getRandomInt(max) {
+    max = Math.floor(Math.random() * Math.floor(max));
+    if (max === 0) max++;
+    return max;
+}
