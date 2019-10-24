9cb361f86b347a1ca38bd309a701279f2c15003f
Patrick Anlanger <anlanger.patrick@gmail.com>
2019-10-24 23:05:02 +0200

    server stuff funzt fürs erste

---
+++ index.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const app = express();
+
+//middlewares
+app.use(express.static('public'));
+
+//routes
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+//Listen on port 3000
+server = app.listen(3000);
+
+//socket.io instantiation
+const io = require("socket.io")(server);
+
+//playerList
+let playerList = [];
+
+
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on("user_join", function (data) {
+        this.username = data;
+        socket.broadcast.emit("user_join", data);
+
+        //tests
+        playerList.push(this.username);
+        console.log(playerList);
+
+        socket.emit("showPlayers", {
+            playerList
+        });
+        socket.broadcast.emit("showPlayers", {
+            playerList
+        });
+        //tests
+    });
+
+    socket.on("chat_message", function (data) {
+        data.username = this.username;
+        socket.broadcast.emit("chat_message", data);
+    });
+
+    socket.on("disconnect", function (data) {
+        socket.broadcast.emit("user_leave", this.username);
+
+        //tests
+        for (i = 0; i < playerList.length; i++) {
+            if (playerList[i] === this.username) {
+                playerList.splice(i, 1);
+            }
+        }
+        // console.log(playerList);
+        socket.emit("showPlayers", {
+            playerList
+        });
+        socket.broadcast.emit("showPlayers", {
+            playerList
+        });
+        //tests
+    });
+
+    // socket.on("myEvent", function(){
+    //     socket.emit("showPlayers", liste);
+    // })
+
+
+    // socket.emit("showPlayers", {
+    //     playerList
+    //   });
+
+})
---
+++ public/dnd.js
@@ -0,0 +1,145 @@
+class Player {
+
+    constructor(name) {
+        this.playerName = name;
+        this.name(name);
+        this.charImage(name);
+        this.dice(name);
+    }
+
+    static playerName(name) {
+        return name;
+    };
+
+
+    name(name) {
+        let nameInput = document.createElement('input');
+        nameInput.setAttribute('class', 'names');
+        nameInput.setAttribute('value', name);
+        document.getElementById('nameContainer').appendChild(nameInput);
+        // return name;
+    }
+
+    charImage(name) {
+        let charDiv = document.createElement('div');
+        charDiv.setAttribute('class', 'chars background');
+        charDiv.setAttribute('id', name);
+        document.getElementById('charContainer').appendChild(charDiv);
+    }
+
+    dice(name) {
+        let diceList = [20, 6, 8, 10, 12, 100];
+        let diceDiv = document.createElement('div');
+        diceDiv.setAttribute('class', 'chars');
+        diceDiv.setAttribute('id', name + '_Dice');
+        document.getElementById('diceContainer').appendChild(diceDiv);
+
+        let dropDown = document.createElement('div');
+        dropDown.setAttribute('class', 'dropdown');
+        let dropbtn = document.createElement('button');
+        dropbtn.setAttribute('class', 'dropbtn');
+        dropDown.appendChild(dropbtn);
+        let dropDownContent = document.createElement('div');
+        dropDownContent.setAttribute('class', 'dropdown-content');
+        dropDown.appendChild(dropDownContent);
+        document.getElementById(name + '_Dice').appendChild(dropDown);
+
+        for (let i = 0; i < diceList.length; i++) {
+            let a = document.createElement('a');
+            a.setAttribute('href', '#');
+            a.innerHTML = diceList[i];
+            a.setAttribute('id', name + i);
+            dropDownContent.appendChild(a);
+            let diceButton = document.getElementById(name + i);
+            diceButton.addEventListener('click', function () {
+                let diceEyes = this.innerHTML;
+                let rollResult = rollDice(diceEyes);
+                let log = document.getElementById('chatLog');
+                log.append(name + " würfelt " + rollResult + " (d" + diceEyes + ")" + "\n");
+                document.getElementById("chatLog").scrollTop = document.getElementById("chatLog").scrollHeight; //textarea autoscroll
+            })
+        }
+
+        let image = document.createElement('img');
+        image.setAttribute('class', 'dice');
+        image.setAttribute('src', 'd20.png');
+        image.setAttribute('alt', 'd20');
+
+        document.getElementById(name + '_Dice').appendChild(image);
+    }
+}
+
+
+
+
+let playerList = []
+const form = document.querySelector("form");
+const input = document.querySelector(".input");
+const messages = document.querySelector(".messages");
+const username = prompt("Gib deinen Charakternamen ein:");
+const socket = io();
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    addMessage(username + ": " + input.value);
+
+    socket.emit("chat_message", {
+        message: input.value
+    });
+
+    input.value = "";
+    return false;
+}, false);
+
+socket.on("chat_message", function (data) {
+    addMessage(data.username + ": " + data.message);
+});
+
+socket.on("user_join", function (data) {
+    addMessage(data + " just joined the chat!");
+});
+
+socket.on("user_leave", function (data) {
+    addMessage(data + " has left the chat.");
+    
+    // for (i = 0; i < data.playerList.length; i++) {
+    //     let player = new Player(data.playerList[i]);
+    // }
+});
+
+socket.on("showPlayers", function (data) {
+    console.log(data.playerList);
+
+    document.getElementById('nameContainer').innerHTML = "";
+    document.getElementById('charContainer').innerHTML = "";
+    document.getElementById('diceContainer').innerHTML = "";
+    for (i = 0; i < data.playerList.length; i++) {
+        let player = new Player(data.playerList[i]);
+    }
+});
+
+addMessage("You have joined the chat as " + username + ".");
+socket.emit("user_join", username);
+
+function addMessage(message) {
+    const li = document.createElement("li");
+    li.innerHTML = message;
+    messages.appendChild(li);
+    window.scrollTo(0, document.body.scrollHeight);
+}
+
+// createBtn = document.getElementById('createCharakterBtn');
+// createBtn.addEventListener('click', function () {
+//     let nameInput = document.getElementById('nameInput').value;
+//     if (nameInput === "") {
+//         alert('charfenster leer');
+//     } else {
+//         let player = new Player(nameInput);
+//     }
+// })
+
+function rollDice(number) {
+    number = Math.floor(Math.random() * number) + 1;
+    return number;
+}
